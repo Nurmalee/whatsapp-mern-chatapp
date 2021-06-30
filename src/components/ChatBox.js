bc4bb170e87418b5b70e2dc36292b82c3c750d01
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-06-30 18:08:11 +0100

    CHATBOX: Created ChatBox Component including Headers and chatBody

---
+++ src/components/ChatBox.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+import { AttachFile, MoreVert, Search } from '@material-ui/icons'
+import { Avatar, IconButton } from '@material-ui/core';
+import my_pics from '../resources/images/Nurudeen.jpg'
+import ChatBody from './ChatBody';
+
+const ChatBox = () => {
+    return (
+        <ChatBoxContainer>
+            <ChatBoxHeader>
+
+                <Avatar src={my_pics} />
+
+                <ActiveChatRoom>
+                    <h2>Room Name</h2>
+                    <p>Last Seen at ...</p>
+                </ActiveChatRoom>
+
+                <Icons>
+                    <IconButton>
+                        <Search />
+                    </IconButton>
+
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </Icons>
+            </ChatBoxHeader>
+
+            <ChatBody />
+        </ChatBoxContainer>
+    )
+}
+
+export default ChatBox
+
+const ChatBoxContainer = styled.div`
+    flex: 0.7;
+    display: flex;
+    flex-direction: column;
+`
+
+const ChatBoxHeader = styled.div`
+    padding: 20px;
+    display: flex;
+    align-items: center;
+`
+
+const ActiveChatRoom = styled.div`
+    flex: 1;
+    padding: 0 20px;
+
+    > h2 {
+        font-size: 16px;
+    }
+
+    > p {
+        font-size: 12px;
+    }
+`
+
+const Icons = styled.div`
+    
+`
