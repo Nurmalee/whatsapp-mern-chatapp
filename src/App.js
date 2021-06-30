b11ede5ee19c7e1aec967c0770155325b8b386ab
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-06-30 18:09:51 +0100

    APP: Component Importation and Inclusions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import styled from 'styled-components'
+import SideBar from './components/SideBar'
+import ChatBox from './components/ChatBox'
+
 const App = () => {
   return (
-    <div>
+    <AppContainer>
+      <AppBody>
+        <SideBar />
+        <ChatBox />
+      </AppBody>
       
-    </div>
+    </AppContainer>
   )
 }
 
 export default App
+
+const AppContainer = styled.main`
+  display: grid;
+  place-items: center;
+  background-color: #dadbd3;
+  min-width: 750px;
+  height: 100vh;
+  min-height: 600px;
+`
+
+const AppBody = styled.div`
+  display: flex;
+  width: 95vw;
+  min-width: 700px;
+  height: 90vh;
+  min-height: 550px;
+  background-color: #ededed;
+  box-shadow: -1px 4px 15px -6px rgba(0, 0, 0, 0.75);
+`
