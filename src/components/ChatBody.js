1e5d11b1d30c11ee610c80207c84e22346286775
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-06-30 18:08:58 +0100

    CHATBDY: Created ChatBody Component including chat Messages

---
+++ src/components/ChatBody.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import chat_background from '../resources/images/whatsapp-chat-background.png'
+
+const ChatBody = () => {
+    return (
+        <ChatBodyContainer>
+            
+        </ChatBodyContainer>
+    )
+}
+
+export default ChatBody
+
+const ChatBodyContainer = styled.div`
+    flex: 1;
+    background-image: url(${chat_background});
+    background-repeat: repeat;
+    background-position: center;
+    overflow-y: scroll;
+    padding: 30px;
+`
