1c7de6f646de71b65721460fd809f0f08fc0fe83
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-07-01 17:37:27 +0100

    CHAT FORM - Created Chat Form Field

---
+++ src/components/ChatForm.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+import { useState } from 'react'
+import { InsertEmoticonOutlined, MicOutlined } from '@material-ui/icons';
+import { IconButton } from '@material-ui/core'
+
+const ChatForm = () => {
+
+    const [message, setMessage] = useState('')
+
+    const handleForm = (e) => {
+        e.preventDefault()
+        setMessage('')
+    }
+
+    return (
+        <ChatFormContainer>
+
+            <IconButton>
+                <InsertEmoticonOutlined />
+            </IconButton>
+            
+
+            <form onSubmit={handleForm}>
+                <input type='text' value={message} onChange={(e) => setMessage(e.target.value)} />
+            </form>
+
+            <IconButton>
+                <MicOutlined />
+            </IconButton>
+
+        </ChatFormContainer>
+    )
+}
+
+export default ChatForm
+
+
+const ChatFormContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 10px 15px;
+    background-color: #ededed;
+
+    > form {
+        border-radius: 20px;
+        flex: 1;
+        padding: 0 10px;
+
+        > input {
+            font-family: 'Roboto', sans-serif;
+            font-size: 16px;
+            font-weight: 300;
+            width: 100%;
+            border-radius: 20px;
+            outline: none;
+            border: none;
+            padding: 10px 20px;
+        }
+    }
+`
