e61d78507534507e3c2a0702047043de03ef72fd
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-07-01 17:36:53 +0100

    CHAT MESSAGE - Edited Chat Message

---
+++ src/components/ChatMessage.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+const ChatMessage = ({message}) => {
+    return (
+        <ChatMessageContainer className={true && `received`}>
+            <span>Lawal Nurudeen</span>
+           
+            <MessageDetails>
+                {message}
+                <span>Thu, 12:59AM</span>
+            </MessageDetails>
+        </ChatMessageContainer>
+    )
+}
+
+export default ChatMessage
+
+const ChatMessageContainer = styled.div`
+
+    .received {
+        background-color: lightgreen;
+    }
+
+    > span {
+        font-size: 11px;
+        padding-left: 10px;
+    }
+
+`
+
+const MessageDetails = styled.p`
+    font-size: 12px;
+    border-radius: 15px;
+    width: fit-content;
+    padding: 10px;
+    margin: 2px 0 15px 0;
+    background-color: white;
+
+    > span {
+        font-size: 11px;
+        margin-left: 25px;
+    }
+
+`
