ce61ac54adea34507108b1b061aef932695ea545
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-07-03 22:06:41 +0100

    ADD ROOM: Implemented Add Room to firebase firestore functionality plus extra performance upgrades

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -23,17 +23,18 @@ const SideBar = () => {
     const [loading, setLoading] = useState(true)
     const [initialRooms, setInitialRooms] = useState([])
     const [chatRooms, setChatRooms] = useState([])
+    const [pendingRoom, setPendingRoom] = useState('')
 
     useEffect(() => {
         setLoading(true)
-        const subscribe = db.collection('chat-rooms').onSnapshot(snapshot => {
+        const subscribe = db.collection('chat-rooms').orderBy('name').onSnapshot(snapshot => {
             const documents = snapshot.docs.map(doc => ({
                 id: doc.id,
                 data: doc.data()
             }))
+            documents.length > 0 && setLoading(false)  
             setInitialRooms(documents)
             setChatRooms(documents)
-            documents.length < 0 && setLoading(false)  
         })
 
         return () => subscribe()
@@ -41,12 +42,34 @@ const SideBar = () => {
 
     const handleRoomSearch = (e) => {
         const roomname = e.target.value
+        setPendingRoom(e.target.value)
+
         if(initialRooms){
             const filteredRooms = initialRooms.filter(room => 
                 room.data.name.toLowerCase().includes(roomname.toLowerCase()) ? room : null
             )
             setChatRooms(filteredRooms)
         }
+    }
+
+    const addRoom = (e) => {
+        e.preventDefault()
+
+        const suffix = ['Room', 'Hub', 'Chat', 'ChatRoom', 'Square', 'Clique', 'Crew']
+        const randomSuffix = suffix[Math.floor(Math.random() * suffix.length)]
+
+        const newRoom = pendingRoom.charAt(0).toUpperCase() + pendingRoom.slice(1)
+       
+        if(pendingRoom && pendingRoom.length > 3 && pendingRoom.length <= 15){
+            db.collection('chat-rooms').add({
+                // name: `${newRoom} Room`
+                name: newRoom + " " + randomSuffix
+            })
+        } else {
+            setChatRooms(initialRooms)
+        }
+
+        setPendingRoom('')
     }
 
     return (
@@ -74,15 +97,24 @@ const SideBar = () => {
             <SideBarBody>
                 <SideBarInput>
                     <Search />
-                    <input type="text" onChange={handleRoomSearch} placeholder="Search or start a new chat" />
+                    <form onSubmit={addRoom}>
+                        <input type="text" onChange={handleRoomSearch} value={pendingRoom} placeholder="Search or start a new chat" />
+                    </form>
+
+                    {(pendingRoom.length >= 1 && pendingRoom.length < 4) ? <span>If adding room, room name must be more than 4 letters</span> : null}
+
+                    {pendingRoom.length >= 15 ? <span>If adding room, room name must be less than 12 letters</span> : null}
                 </SideBarInput>
 
                 <SideBarRooms>
+                    {/* {
+                        chatRooms.length < 0 && <p>Add a new group to start chatting</p>
+                    } */}
                     {
                         chatRooms.map(room => <SideBarRoom key={room.id} id={room.id} roomname={room.data.name} />)
                     }
                     {
-                        loading && [1,2,3,4,5, 7,8,9,10,11].map(n => <RoomSkeleton key={n} id={n} />)
+                        loading && [1,2,3,4,5, 7,8,9,10,11,12].map(n => <RoomSkeleton key={n} id={n} />)
                     }
                 </SideBarRooms>
             </SideBarBody>
@@ -125,30 +157,43 @@ const SideBarInput = styled.div`
     align-items: center;
     background-color: white;
     border-radius: 20px;
+    position: relative;
 
     > .MuiSvgIcon-root {
         font-size: 20px;
         color: grey;
     }
 
-    > input {
-        font-family: 'Roboto', sans-serif;
-        font-weight: 100;
+    > form {
         flex: 1;
-        border-radius: 20px;
-        padding: 1px 10px;
-        padding-left: 15px;
-        color: grey;
-        font-size: 16px;
-        border: none;
-        outline: none;
+
+        > input {
+            font-family: 'Roboto', sans-serif;
+            font-weight: 100;
+            width: 100%;
+            border-radius: 20px;
+            padding: 1px 10px;
+            padding-left: 15px;
+            color: grey;
+            font-size: 16px;
+            border: none;
+            outline: none;
+        }
+    }        
+
+    > span {
+        position: absolute;
+        font-size: 9px;
+        top: 36px;
+        left: 2px;
+        z-index: 20;
     }
 `
 
 const SideBarRooms = styled.div`
     background-color: white;
     position: absolute;
-    top: 52px;
+    top: 57px;
     right: 0;
     left: 0;
     bottom: 0;
@@ -165,5 +210,9 @@ const SideBarRooms = styled.div`
     &::-webkit-scrollbar-thumb {
         background-color: darkgrey;
         border-radius: 5px;
+    }
+
+    > p {
+        z-index: 20
     }
 `
