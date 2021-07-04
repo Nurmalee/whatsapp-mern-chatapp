3b5a412b3b7c3923fe7297e3a6b0153e47560bc7
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-07-04 01:36:08 +0100

    ROUTING

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import styled from 'styled-components'
 import SideBar from './components/SideBar'
 import ChatBox from './components/ChatBox'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <AppContainer>
-        <SideBar />
-        <ChatBox />
+
+      <AppBody>
+        <Router>
+          <SideBar />
+          <Switch>
+            {/* <Route path='/'>
+              <ChatBox />
+            </Route> */}
+            <Route path='/rooms/:roomId' component={ChatBox} />
+          </Switch>
+        </Router>
+      </AppBody>
     </AppContainer>
   )
 }
 
 export default App
 
-const AppContainer = styled.div`
+const AppContainer = styled.main`
+  display: grid;
+  place-items: center;
+`
+
+const AppBody = styled.div`
   display: flex;
+  width: 100vw;
   min-width: 800px;
   height: 100vh;
   min-height: 550px;
